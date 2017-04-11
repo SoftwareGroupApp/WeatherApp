2f3347fde29a4f16230178501f286cc1dad0a5b4
eml54 <eml54@txstate.edu>
2017-04-11 13:08:46 -0500

    Split the code into functional areas,

--- app/src/main/java/com/hannahburzynski/weatherapp/CurrentWeather.java
+++ app/src/main/java/com/hannahburzynski/weatherapp/CurrentWeather.java
@@ -15,6 +15,9 @@ public class CurrentWeather {
     private double humidity;
     private double pressure;
     private String city;
+    private double lat;
+    private double lon;
+    public String getFormattedDate;
 
     public String getCity() {
         return city;
@@ -28,21 +31,9 @@ public class CurrentWeather {
         return time;
     }
 
-    // Get the formatted time from a UNIX time string value
-    public String getFormattedTime() {
-        SimpleDateFormat formatter = new SimpleDateFormat("h:mm a");
-        formatter.setTimeZone(TimeZone.getTimeZone(getCity()));
-
-        // Date time is in milliseconds
-        Date dateTime = new     Date(getTime() * 1000);
-        // Convert Date to String
-        String timeString = formatter.format(dateTime);
-
-        return timeString;
-    }
-
     public void setTime(long time) {
-        this.time = time;
+        // Date time is in milliseconds
+        this.time = (time * 1000L);
     }
 
     public int getTemperature() {
@@ -64,4 +55,32 @@ public class CurrentWeather {
     public double getPressure() { return pressure; }
 
     public void setPressure(double pressure) { this.pressure = pressure; }
+
+    public double getLat() { return lat; }
+
+    public void setLat(double lat) { this.lat = lat; }
+
+    public double getLon() { return lon; }
+
+    public void setLon(double lon) { this.lon = lon; }
+
+    // Get the formatted time from a UNIX time string value
+    public String getFormattedTime() {
+        SimpleDateFormat formatter = new SimpleDateFormat("h:mm a");
+        formatter.setTimeZone(TimeZone.getTimeZone(MainActivity.locTimeZone));
+        Date dateTime = new Date(getTime());
+        // Convert Date to String
+        String timeString = formatter.format(dateTime);
+        return timeString;
+    }
+
+    public String getFormattedDate(){
+        SimpleDateFormat formatDate = new SimpleDateFormat("EEE, MMM d, yyyy");
+        formatDate.setTimeZone(TimeZone.getTimeZone((MainActivity.locTimeZone)));
+        Date dateTime = new Date(getTime());
+        // Convert Date to String
+        String timeString = formatDate.format(dateTime);
+        return timeString;
+
+    }
 }
--- app/src/main/java/com/hannahburzynski/weatherapp/MainActivity.java
+++ app/src/main/java/com/hannahburzynski/weatherapp/MainActivity.java
@@ -46,9 +46,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView dateTextView;
     private String query;
     public static String locTimeZone;
-    //private EditText searchBoxEditText;
-   // private static String city2Get;
 
+/////////////////////////////// o n C r e a t e ////////////////////////////////////////////
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,19 +57,73 @@ public class MainActivity extends AppCompatActivity {
         onStart();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+/////////////////////////////// o n S t a r t //////////////////////////////////////////////
+
     protected void onStart() {
         super.onStart();
         onResume();
     }
 
+/////////////////////////////// o n R e s u m e ////////////////////////////////////////////
 
     protected void onResume() {
         super.onResume();
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+        intializeUI();
+        getWeather(query);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int selectedItemId = item.getItemId();
+        if(selectedItemId == R.id.action_search){
+                Toast.makeText(this, "Search clicked", Toast.LENGTH_LONG).show();
+                return true;
+            }
+           return super.onOptionsItemSelected(item);
+        }
 
+/////////////////////////////// L i s t e n e r ////////////////////////////////////////////
+
+    class OnCitySelectedListener implements AdapterView.OnItemSelectedListener {
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id){
+            String city2Get = "q=" + parent.getSelectedItem();
+            Toast.makeText(parent.getContext(), parent.getItemAtPosition(pos).toString(), Toast.LENGTH_SHORT).show();
+        getWeather(city2Get);
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+            // DO nothing
+        }
+    }
+
+/////////////////////////////// M E T H O D S /////////////////////////////////////////////
+
+    private String getCityByLoc(){
+        double locLat =  30.570851; //50.751244; //
+        double locLong = -97.653652; //37.618423; //
+        return "lat=" + locLat + "&lon=" + locLong;
+    }
+
+    private String localTimeZone(){
+        //this gets the local time zone from the device but just ru
+        String timeZoneText =  TimeZone.getDefault() + "";
+        int tzLeft = timeZoneText.indexOf("id=") + 4;
+        int txRight = timeZoneText.indexOf(",mRawOffset") - 1;
+        return timeZoneText.substring(tzLeft, txRight);
+    }
+
+    protected void intializeUI(){
         // Initialize UI
         timeTextView = (TextView) findViewById(R.id.timeTextView);
         temperatureTextView = (TextView) findViewById(R.id.temperatureTextView);
@@ -84,64 +137,6 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.city_array,android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(new OnCitySelectedListener());
-
-
-        // URL parameters
-        final String APP_ID = "ddd38c0b3a7b986b4f6c036e8c9081bc";
-        //by city
-        // query = getCityByName();
-        //by location
-      //  query = getCityByLoc();
-      //  LocationManager locationManager = (LocationManager)
-      //          getSystemService(Context.LOCATION_SERVICE);
-        String forecastURL = "http://api.openweathermap.org/data/2.5/weather?" + query + "&appid=" + APP_ID;
-
-        // Check if network is available before making request
-        if (isNetworkAvailable()) {
-            OkHttpClient client = new OkHttpClient();
-            // Build a request that the client will send to the server
-            Request request = new Request.Builder().url(forecastURL).build();
-            // Put request in a call object
-            Call call = client.newCall(request);
-            // Place call on the queue to be executed in a background thread
-            // Callbacks are used to communicate between the background thread and main thread
-            call.enqueue(new Callback() {
-                @Override
-                public void onFailure(Call call, IOException e) {
-
-                }
-
-                @Override
-                public void onResponse(Call call, Response response) throws IOException {
-                    // Store response from call
-                    try {
-                        String jsonData = response.body().string();
-                        Log.v(TAG, jsonData);
-                        if (response.isSuccessful()) {
-                            // Store requested data in current weather object
-                            currentWeather = getCurrentDetails(jsonData);
-                            // Update the UI elements
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    updateDisplay();
-                                }
-                            });
-                        } else {
-                            alertUserAboutError();
-                        }
-                    } catch (IOException e) {
-                        Log.e(TAG, "Exception caught: ", e);
-                    }
-                      catch (JSONException e) {
-                          Log.e(TAG, "Exception caught: ", e);
-                      }
-                }
-            });
-        }
-        else {
-            Toast.makeText(this, R.string.network_unavailable_message,Toast.LENGTH_LONG).show();
-        }
     }
 
     private void updateDisplay() {
@@ -154,6 +149,76 @@ public class MainActivity extends AppCompatActivity {
         latTextView.setText(currentWeather.getLat() + "");
         longTextVew.setText(currentWeather.getLon() + "");
         dateTextView.setText(currentWeather.getFormattedDate());
+    }
+
+    private boolean isNetworkAvailable() {
+        boolean isAvailable = false;
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        // Determine if network info is present and active, need permission to access network state
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        // Check if network is present and connected
+        if(networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        }
+        return isAvailable;
+    }
+
+    protected void getWeather(String query){
+        final String APP_ID = "ddd38c0b3a7b986b4f6c036e8c9081bc";
+        String forecastURL = "http://api.openweathermap.org/data/2.5/weather?" +
+                query + "&appid=" + APP_ID;
+        // Check if network is available before making request
+        if (isNetworkAvailable()) {
+            OkHttpClient client = new OkHttpClient();
+            // Build a request that the client will send to the server
+            Request request = new Request.Builder().url(forecastURL).build();
+            // Put request in a call object
+            Call call = client.newCall(request);
+            // Place call on the queue to be executed in a background thread
+            // Callbacks are used to communicate between the background thread and main thread
+            call.enqueue(new Callback() {
+
+                @Override
+                public void onFailure(Call call, IOException e) { }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    // Store response from call
+                    try {
+                        String jsonData = response.body().string();
+                        Log.v(TAG, jsonData);
+                        if (response.isSuccessful()) {
+                            // Store requested data in current weather object
+                            currentWeather = getCurrentDetails(jsonData);
+                            // Update the UI elements
+                            runOnUiThread(new Runnable() {
+
+                                @Override
+                                public void run() {
+                                    updateDisplay();
+                                }
+                            });
+                        } else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    }
+                    catch (JSONException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    }
+                }
+            });
+        }
+        else {
+            Toast.makeText(this, R.string.network_unavailable_message,Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        // Show error dialog
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 
     private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
@@ -172,80 +237,6 @@ public class MainActivity extends AppCompatActivity {
         currentWeather.setCity(forecast.getString("name"));
         currentWeather.setLat(coord.getDouble("lat"));
         currentWeather.setLon(coord.getDouble("lon"));
-
-
         return currentWeather;
-    }
-
-    private boolean isNetworkAvailable() {
-        boolean isAvailable = false;
-
-        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        // Determine if network info is present and active, need permission to access network state
-        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
-
-        // Check if network is present and connected
-        if(networkInfo != null && networkInfo.isConnected()) {
-            isAvailable = true;
-        }
-
-        return isAvailable;
-    }
-
-    private void alertUserAboutError() {
-        AlertDialogFragment dialog = new AlertDialogFragment();
-        // Show error dialog
-        dialog.show(getFragmentManager(), "error_dialog");
-    }
-
-
-
-    private String getCityByLoc(){
-        double locLat =  30.570851; //50.751244; //
-        double locLong = -97.653652; //37.618423; //
-        return "lat=" + locLat + "&lon=" + locLong;
-    }
-
-    private String localTimeZone(){
-        //this gets the local time zone from the device but just ru
-        String timeZoneText =  TimeZone.getDefault() + "";
-        int tzLeft = timeZoneText.indexOf("id=") + 4;
-        int txRight = timeZoneText.indexOf(",mRawOffset") - 1;
-        return timeZoneText.substring(tzLeft, txRight);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int selectedItemId = item.getItemId();
-        if(selectedItemId == R.id.action_search){
-                Toast.makeText(this, "Search clicked", Toast.LENGTH_LONG).show();
-                return true;
-            }
-           return super.onOptionsItemSelected(item);
-        }
-
-
-    class OnCitySelectedListener implements AdapterView.OnItemSelectedListener {
-        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id){
-            String city2Get = parent.getSelectedItem() + "";
-            Toast.makeText(parent.getContext(), parent.getItemAtPosition(pos).toString(), Toast.LENGTH_SHORT).show();
-        getCityByName(city2Get);
-        }
-        private String getCityByName(String city2Get){
-            String cityName =  city2Get;
-            return "q=" + cityName;
-        }
-
-        @Override
-        public void onNothingSelected(AdapterView<?> parent) {
-            // DO nothing
-        }
     }
 }
